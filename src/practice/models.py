3068dcef4eca3c59694dd7822953cb0340cef1ad
Tyler Herndon <tylerherndon89@gmail.com>
2025-08-28 17:21:30 -0700

    Hopefully this works

---
+++ src/practice/models.py
@@ -0,0 +1,14 @@
+# after changes to models.py, run
+# python manage.py makemigrations
+# and
+# python manage.py migrate
+
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    title       = models.CharField(max_length = 120) # max_length = required
+    description = models.TextField(blank=True, null=True)
+    price       = models.DecimalField(decimal_places=2, max_digits = 10000)
+    summary     = models.TextField()
+    featured    = models.BooleanField() # null=True, default=True
